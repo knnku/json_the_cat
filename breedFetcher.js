574875e80f71ad5f203ab9cb8f9a08213996afc4
Kenniku Tolato <kenniku_you@ymail.com>
2023-08-13 02:53:03 +0000

    Add request and output functionality to app.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,15 @@
-const request  = require('request');
+const request = require("request");
 
+request(
+  "https://api.thecatapi.com/v1/breeds/search?q=sib",
+  (error, reponse, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    const data = JSON.parse(body);
+
+    console.log(data[0].description, typeof data);
+  }
+);
