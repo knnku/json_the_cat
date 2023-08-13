0fc8429bccb86941b9b693776ebde706af5de7a1
Kenniku Tolato <kenniku_you@ymail.com>
2023-08-13 03:19:17 +0000

    Cleaning up and adding error catches.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,17 @@
 const request = require("request");
+const breed = process.argv.slice(2);
 
 request(
-  "https://api.thecatapi.com/v1/breeds/search?q=sib",
-  (error, reponse, body) => {
-    if (error) {
-      console.log(error);
-      return;
+  "https://api.thecatapi.com/v1/breeds/search?q=" + breed,
+  (error, response, body) => {
+    if (error && error.code === "ENOTFOUND") {
+      return console.log("Error: There was an error with the server.");
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      return console.log("The breed entered does not exist.");
     }
 
-    const data = JSON.parse(body);
-
-    console.log(data[0].description, typeof data);
+    console.log(data[0].description);
   }
 );
